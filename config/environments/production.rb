738d68a53dbed10a64f343b572da9932b0c43634
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2013-05-12 14:58:11 +0100

    configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,10 @@ PromanXiv::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    address: "smtp.madrillapp.com",
+    port: 25,
+    user_name: ENV["MADRILL_USERNAME"],
+    password: ENV["MANDRILL_API_KEY"]
   }
 
 
